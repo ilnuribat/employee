2b25aaadb20e8116864f745f6d3f6ab27a7f749d
ilnuribat <ilnuribat@gmail.com>
2018-03-20 14:17:38 +0300

    server is starting inside dokcer

---
+++ src/days/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const Koa = require('koa');
+const Bluebird = require('bluebird');
+require('dotenv').config();
+
+Bluebird.promisifyAll(http.Server.prototype);
+
+const app = new Koa();
+const server = http.createServer(app.callback());
+
+app.use((ctx) => {
+  ctx.body = {
+    result: 'OK',
+  };
+});
+
+async function start() {
+  await server.listen(process.env.HTTP_PORT_DAYS);
+  console.log(`days' service started at port ${process.env.HTTP_PORT_DAYS}`);
+}
+
+start();
