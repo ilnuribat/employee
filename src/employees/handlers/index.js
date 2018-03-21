c940ea61ff67d11c8ceaf0ee2d05a8e62a251647
ilnuribat <ilnuribat@gmail.com>
2018-03-21 10:19:17 +0300

    edit employee

--- src/employees/handlers/index.js
+++ src/employees/handlers/index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const config = require('../knexfile');
 const knex = require('knex')(config[process.env.NODE_ENV]);
 
@@ -5,6 +6,11 @@ async function ADD_EMPLOYEE(data) {
   return knex('Employee').insert(data).returning('*');
 }
 
+async function EDIT_EMPLOYEE(data) {
+  return knex('Employee').update(_.omit(data, ['id'])).returning('*');
+}
+
 module.exports = {
   ADD_EMPLOYEE,
+  EDIT_EMPLOYEE,
 };
